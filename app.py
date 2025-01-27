9ddd9ab953a16ec1111e5a9183afbf14d24c960a
Daniel Salin <daniel.salin@gmail.com>
2025-01-27 18:29:28 +0100

    Accomplish basic CCV retrival via API

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # File to develop and use testing of YT API
 from config import Config
 from googleapiclient.discovery import build
+import pprint
+import datetime
 
 # Stores the API KEY
 conf = Config()
@@ -9,11 +11,23 @@ developer_key = conf.YTT_API_KEY
 # Create service
 youtube = build("youtube", "v3", developerKey=developer_key)
 
-request = youtube.channels().list(
-    part = "statistics",
-    forUsername="schafer5"
+channel_request = youtube.search().list(
+    part="snippet,id, contentDetails",
+    channelId="UCuI_opAVX6qbxZY-a-AxFuQ",
+    eventType="live",
+    type="video"
 )
 
-response = request.execute()
+video_request = youtube.videos().list(
+    part="liveStreamingDetails",
+    id="vhxtAtZnTb8"
+)
 
-print(response)
+#channel_response = request.execute()
+video_response = video_request.execute()
+livestreamDetails = video_response["items"][0]["liveStreamingDetails"]
+ccv = livestreamDetails["concurrentViewers"]
+
+# DAILY QUOTA 10 000 UNITS
+# SEARCH LIST REQUEST = 100 UNIT -channel_request
+# VIDEO REQUEST = 1 UNIT -video_request
